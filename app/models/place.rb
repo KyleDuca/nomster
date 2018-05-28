6f78b29bb57969837a735ec9568789ffb71a8e7c
Kyle Duca <kyleduca@gmail.com>
2018-05-28 21:41:44 +0000

    set up maps intergration

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,3 +1,9 @@
 class Place < ApplicationRecord
   belongs_to :user
+  geocoded_by :address
+  after_validation :geocoded_by
+  
+  validates :name, length: { minimum: 3 }
+  validates :address, presence: { minimum: 10 }
+  validates :description, length: { in: 5..2000 }
 end
