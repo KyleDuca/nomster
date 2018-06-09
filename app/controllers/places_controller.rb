1019837e40d31036d7f4e508955cce0c26a7f2fa
Kyle Duca <kyleduca@gmail.com>
2018-06-09 19:40:00 +0000

    set up email responder

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,16 +1,14 @@
 class PlacesController < ApplicationController
-
-  before_action :authenticate_user!, only: [ :new, :create, :edit, :update, :destroy ]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   def index
-    @places = Place.all.paginate(page: params[:page], per_page: 5)
+    @places = Place.paginate(:page => params[:page], :per_page => 10)
   end
-
 
   def new
     @place = Place.new
   end
-  
+
   def create
     @place = current_user.places.create(place_params)
     if @place.valid?
@@ -23,39 +21,41 @@ class PlacesController < ApplicationController
   def show
     @place = Place.find(params[:id])
     @comment = Comment.new
+    @photo = Photo.new
   end
 
   def edit
     @place = Place.find(params[:id])
+
     if @place.user != current_user
-      return render plain: 'Not Allowed', status: :forbidden
+      return render text: 'Not Allowed', status: :forbidden
     end
+
   end
 
   def update
     @place = Place.find(params[:id])
     if @place.user != current_user
-      return render plain: 'Not Allowed', status: :forbidden
+      return render text: 'Not Allowed', status: :forbidden
     end
 
     @place.update_attributes(place_params)
-
     if @place.valid?
       redirect_to root_path
     else
-      render :new, status: :unprocessable_entity
+      render :edit, status: :unprocessable_entity
     end
   end
 
   def destroy
     @place = Place.find(params[:id])
     if @place.user != current_user
-      return render plain: 'Not Allowed', status: :forbidden
+      return render text: 'Not Allowed', status: :forbidden
     end
     @place.destroy
     redirect_to root_path
   end
-
+  
   private
 
   def place_params
---
+++ app/models/photo.rb
@@ -0,0 +1,10 @@
+class Photo < ApplicationRecord
+  belongs_to :user
+  belongs_to :place
+  mount_uploader :picture, PictureUploader
+
+  validates :caption, presence: true
+  validates :picture, presence: true
+
+
+end
